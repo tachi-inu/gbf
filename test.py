f235b391a64cff5ad5e985861ed33234b0a1df0a
tachi-inu <yuit.805@gmail.com>
2023-04-12 22:56:56 +0900

    neru

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
-class ability:
-    a
+chara_type = ["Human", "Erune", "Draph", "Harvin", "Primal", "Other"]
+class skill():
+    def __init__(self, name, category, limit):
+        self.name = name
+        self.cat = category
+        self.limit = limit
+
+valuable = skill()
