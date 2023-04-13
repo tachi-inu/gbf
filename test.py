efb7c7c7573c66b441c30abdc742c03d31e98911
tachi-inu <yuit.805@gmail.com>
2023-04-13 11:58:12 +0900

    second

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 chara_type = ["Human", "Erune", "Draph", "Harvin", "Primal", "Other"]
+skill_type = ["omega_atk", "omega_enm", "omega_stm", "omega_vrt", "omega_ex", "opt_atk", "opt_enm", "opt_stm",
+              "opt_ex", "unk"]
 class skill():
-    def __init__(self, name, category, limit):
+    def __init__(self, name:str, category, limit:int, aura:bool):
         self.name = name
         self.cat = category
         self.limit = limit
+        self.aura = aura
 
 valuable = skill()
